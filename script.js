edd3b6e2ef8b7864835948e8444cbbc0ba91d4f1
Maryam-Tariq-Dev <engr.maryamtariq@gmail.com>
2024-11-04 07:26:09 +0500

    pushing all data

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Toggle the mobile navigation menu
+document.querySelector('.menu-toggle').addEventListener('click', function () {
+    document.querySelector('.nav-links').classList.toggle('active');
+});
